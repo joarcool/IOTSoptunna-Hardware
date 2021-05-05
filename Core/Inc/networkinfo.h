cdac104db1e0c51770ef5c95279db2eb811e768b
Joar Edling <jorre98@gmail.com>
2021-05-05 16:09:31 +0200

    La till så att den kan skicka data

--- Core/Inc/networkinfo.h
+++ Core/Inc/networkinfo.h
@@ -10,7 +10,7 @@
 
 static char SSID[] = "Ostomelett"; //Your SSID, example: "MyWiFi"
 static char PWD[] = "JMEP2074"; //Your Password, example: "Password123"
-static char connection_type[] = "TCP"; //The connection type, example: "TCP", "UDP"...
+static char connection_type[] = "SSL"; //The connection type, example: "TCP", "UDP"...
 static char remote_ip[] = "192.168.1.129"; //If you want to connect to a IP adress
 static char remote_port[] = "8888"; //The port, example "8888"
 
--- Core/Src/test.c
+++ Core/Src/test.c
@@ -9,17 +9,13 @@
 #include "ESP8266.h"
 #include "usart.h"
 #include "gpio.h"
-/*static uint8_t rx_variable;
-static char rx_buffer[RX_BUFFER_SIZE];
-static uint8_t rx_buffer_index = 0;
-static bool error_flag = false;
-static bool fail_flag = false;*/
+#include "networkinfo.h"
 
 void Test_program()
 {
 	Test_wifi_connect();
 	Test_connection();
-	Test_sendData();
+	//Test_sendData();
 }
 
 void Test_wifi_connect()
@@ -31,19 +27,32 @@ void Test_wifi_connect()
 	esp8266_send_data("hej");*/
 
 	esp8266_initialize();
+	return;
 }
 
 void Test_connection()
 {
-	eps8266_connection();
+	/*eps8266_connection();
+	return;*/
+	char tcp_connect[30];
+	sprintf(tcp_connect, "%s\"%s\",\"%s\",%s\r\n", ESP8266_AT_START, connection_type, remote_ip, remote_port);
+	//HAL_UART_Transmit(&huart2, (uint8_t *) tcp_connect, strlen(tcp_connect), 100); //For testing
+	esp8266_sendCommand(tcp_connect);
+
+	char send_mode[] = "AT+CIPSEND=7\r\n";
+	esp8266_sendCommand(send_mode);
+	HAL_UART_Transmit(&huart4, (uint8_t*) "hej\r\n", strlen("hej\r\n"), 100);
+
+	char send_stop[] = "AT+CIPCLOSE=0\r\n";
+	esp8266_sendCommand(send_stop);
+	return;
 }
 
 void Test_sendData()
 {
-	char send_mode[] = "AT+CIPSEND=12\r\n";
+	char send_mode[] = "AT+CIPSEND=7\r\n";
 
-	HAL_UART_Transmit(&huart4, (uint8_t*) send_mode, strlen(send_mode), 100);
+	esp8266_sendCommand(send_mode);
 
-	//HAL_UART_Transmit(&huart2, (uint8_t *) set_ESP8266_mode, strlen(set_ESP8266_mode), 100);
-	//HAL_UART_Transmit(&huart4, (uint8_t*) "hej", strlen("hej"), 100);
+	HAL_UART_Transmit(&huart4, (uint8_t*) "hej\r\n", strlen("hej\r\n"), 100);
 }
