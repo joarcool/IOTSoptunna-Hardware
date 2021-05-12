396c4979b52ad39c3d952c6c3057d4fa50335b50
joarcool <55349252+joarcool@users.noreply.github.com>
2021-05-12 15:07:27 +0200

    Fixade så att man kan skicka till hemsidan

--- Core/Inc/networkinfo.h
+++ Core/Inc/networkinfo.h
@@ -11,7 +11,7 @@
 static char SSID[] = "TN_5GHz_18183F"; //Your SSID, example: "MyWiFi"
 static char PWD[] = "YRED6KAWUX"; //Your Password, example: "Password123"
 static char connection_type[] = "TCP"; //The connection type, example: "TCP", "UDP"...
-static char remote_ip[] = "192.168.10.217"; //If you want to connect to a IP adress
-static char remote_port[] = "8888"; //The port, example "8888"
+static char remote_ip[] = "smartbin.azurewebsites.net"; //Connects to a remote website or ip
+static char remote_port[] = "80"; //The port, example http has "80"
 
 #endif /* INC_NETWORKINFO_H_ */
