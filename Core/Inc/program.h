4ad3722067881032d658ec0155adbc0c6165e777
joarcool <55349252+joarcool@users.noreply.github.com>
2021-05-18 12:44:53 +0200

    La till program filen

---
+++ Core/Inc/program.h
@@ -0,0 +1,23 @@
+/*
+ * program.h
+ *
+ *  Created on: May 17, 2021
+ *      Author: jorre
+ */
+
+#ifndef INC_PROGRAM_H_
+#define INC_PROGRAM_H_
+
+#include <stdbool.h>
+#include "stdint.h"
+#include "main.h"
+#include "usart.h"
+#include "gpio.h"
+#include "ESP8266.h"
+#include "networkinfo.h"
+#include "ultrasonic.h"
+#include <string.h>
+
+void program_main(void);
+
+#endif /* INC_PROGRAM_H_ */
